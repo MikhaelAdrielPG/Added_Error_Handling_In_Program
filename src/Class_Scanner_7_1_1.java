4f1ab281ee370ddf3fba54fcb8ae1c43f6282733
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2023-07-26 12:36:13 +0700

    mengubah code ke dalam bentuk function

--- src/Class_Scanner_7_1_1.java
+++ src/Class_Scanner_7_1_1.java
@@ -5,46 +5,51 @@ public class Class_Scanner_7_1_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Masukkan nama Anda : ");
-        String name = scanner.nextLine(); // Membaca baris teks dari input pengguna
+        String name = inputName(scanner);
+        int age = validationAge(scanner);
+        boolean isMarried = validationBoolean(scanner, "Apakah Anda sudah menikah ? (true/false): ");
 
-        int age = 0; // Inisialisasi umur dengan nilai default
-        boolean isMarried = false; // Inisialisasi isMarried dengan nilai default
+        System.out.println("Hallo " + name);
+        System.out.println("Usia mu " + age + " tahun");
+        System.out.print("Dan kamu ");
+        if (isMarried) {
+            System.out.print("sudah ");
+        } else {
+            System.out.print("belum ");
+        }
+        System.out.print("menikah");
+    }
 
+    public static String inputName(Scanner scanner) {
+        System.out.print("Masukkan nama Anda : ");
+        return scanner.nextLine(); // Membaca baris teks dari input pengguna
+    }
+
+    public static int validationAge(Scanner scanner) {
         // Error handling untuk input umur
         while (true) {
             try {
-                System.out.println("Masukkan umur Anda : ");
-                age = scanner.nextInt(); // Membaca nilai integer dari input pengguna
-                break; // Keluar dari loop jika input umur valid
+                System.out.print("Masukkan umur Anda : ");
+                return scanner.nextInt(); // Membaca nilai integer dari input pengguna
             } catch (InputMismatchException e) {
                 // Jika terjadi InputMismatchException pada input umur, tangkap dan cetak pesan kesalahan
                 System.out.println("Input yang Anda berikan bukan angka. Coba lagi.");
                 scanner.nextLine(); // Membaca karakter newline yang tersisa
             }
         }
+    }
 
+    public static boolean validationBoolean(Scanner scanner, String message) {
         // Error handling untuk input isMarried
         while (true) {
             try {
-                System.out.println("Apakah Anda sudah menikah ? (true/false): ");
-                isMarried = scanner.nextBoolean(); // Membaca nilai boolean dari input pengguna
-                break; // Keluar dari loop jika input isMarried valid
+                System.out.print("Apakah Anda sudah menikah ? (true/false): ");
+                return scanner.nextBoolean(); // Membaca nilai boolean dari input pengguna
             } catch (InputMismatchException e) {
                 // Jika terjadi InputMismatchException pada input isMarried, tangkap dan cetak pesan kesalahan
                 System.out.println("Input yang Anda berikan bukan true/false. Coba lagi.");
                 scanner.nextLine(); // Membaca karakter newline yang tersisa
             }
         }
-
-        System.out.println("Hallo " + name);
-        System.out.println("Usia mu " + age + " tahun");
-        System.out.println("Dan kamu ");
-        if (isMarried) {
-            System.out.println("sudah ");
-        } else {
-            System.out.println("belum ");
-        }
-
     }
 }
